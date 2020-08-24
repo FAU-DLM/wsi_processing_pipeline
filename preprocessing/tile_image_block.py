7b667538d1e6a618c7877d06d8a5893d70c2f2d3
ChristophNeuner <christoph_neuner@web.de>
2020-08-24 08:48:56 +0000

    updated reference of Tile class according to its new location in shared module

--- preprocessing/tile_image_block.py
+++ preprocessing/tile_image_block.py
@@ -1,7 +1,7 @@
 import fastai
 from fastai.vision.all import *
 import wsi_processing_pipeline
-from wsi_processing_pipeline.tile_extraction import slide, filter, tiles, util
+from wsi_processing_pipeline.shared import slide, filter, tiles, util
 from wsi_processing_pipeline.preprocessing.name_getter import NameGetter
 from wsi_processing_pipeline.preprocessing.objects import NamedObject
 import shared
@@ -9,7 +9,7 @@ import shared
 
 class TileImage(Tuple):
     '''
-    This class creates a TensorImage from a wsi_processing_pipeline.tile_extraction.tiles.Tile object.
+    This class creates a TensorImage from a wsi_processing_pipeline.shared.tiles.Tile object.
     additionally the show method enables to display the Tile object as an image
     '''
     @classmethod
@@ -43,7 +43,7 @@ def label_tl_image(f):
     return f.classification_labels
                     
 
-def tile_image(tl:wsi_processing_pipeline.tile_extraction.tiles.Tile)->fastai.vision.data.Image:
+def tile_image(tl:wsi_processing_pipeline.shared.tiles.Tile)->fastai.vision.data.Image:
     '''
     This function is just for showcasing.
     It takes a tile object (wsi-preprocessing pipeline) and returns a WsiImage for displaying purposes 
@@ -55,7 +55,7 @@ class TileTransform(Transform):
     ''' A subclass of fastai Transform. Takes a TileObject and applies the image transform as taking tile object and returning 
     WsiImage aka PIL Image transformation.
     '''
-    def __init__(self, tl:wsi_processing_pipeline.tile_extraction.tiles.Tile, splits):
+    def __init__(self, tl:wsi_processing_pipeline.shared.tiles.Tile, splits):
         self.tl= tl
        
     def encodes(self,f):                  
@@ -71,76 +71,3 @@ def show_batch(x:TileImage, y, samples, ctxs=None, max_n=6, nrows=None, ncols=2,
 
 def TileImageBlock(): 
     return TransformBlock(type_tfms=TileImage.create, batch_tfms=IntToFloatTensor)
-
-
-
-####
-# should be obsolete, but was not 100% sure, if it can be deleted
-####
-
-#def label_ws_image(f):    
-#            name=f.classification_labels
-#                
-#            return name    
-#
-#def wsi_image(tl:wsi_processing_pipeline.tile_extraction.tiles.Tile,                           
-#                          )->fastai.vision.data.Image:
-#            '''This function is just for showcasing.
-#            It takes a tile object (wsi-preprocessing pipeline) and returns a WsiImage for displaying purposes '''
-#                       
-#            name=tl.classification_labels     
-#            img=WsiImage.create(tl)
-#            return WsiImage(img,name)
-#
-#        
-#        
-#        
-#class WsiImage(Tuple):
-#    '''This class can 
-#    create a PIL Image from a Tile object.
-#    additionally the 
-#    show method enables to display the Tile object as an image'''
-#    @classmethod
-#    def create(cls, f):         
-#        wsi_path=f.wsi_path
-#        x=f.get_x()
-#        y=f.get_y()         
-#        width = f.get_width()
-#        height = f.get_height()
-#        level = f.level
-#        tile = tiles.ExtractTileFromWSI(path=wsi_path, x=x, y=y, width=width, height=height, level=level)        
-#        tile = TensorImage(image2tensor(tile))
-#        #tile=PILImage.create(tile)
-#        return tile
-#    
-#    def show(self, ctx=None, **kwargs):    
-#        img,title = self
-#        if not isinstance(img, Tensor):            
-#            t = tensor(img)
-#            t = t.permute(2,0,1)
-#        else: t = img
-#        #line = t.new_zeros(t.shape[0], t.shape[1], 10)
-#        #print(type(t))
-#        return show_image(t,title=title, ctx=ctx, **kwargs)
-#    
-#### custom Transforms ####    
-#class WsiTransform(Transform): 
-#    ''' A subclass of fastai Transform. Takes a TileObject and applies the image transform as taking tile object and returning 
-#    WsiImage aka PIL Image transformation.
-#    '''
-#    def __init__(self, tl:wsi_processing_pipeline.tile_extraction.tiles.Tile, splits):
-#        self.tl= tl
-#       
-#    def encodes(self,f):          
-#        
-#        return wsi_image(f)   
-#
-#@typedispatch
-#def show_batch(x:WsiImage, y, samples, ctxs=None, max_n=6, nrows=None, ncols=2, figsize=None, **kwargs):     
-#    if figsize is None: figsize = (ncols*6, max_n//ncols * 3)
-#    if ctxs is None: ctxs = get_grid(min(x[0].shape[0], max_n), nrows=None, ncols=ncols, figsize=figsize)      
-#    for i,ctx in enumerate(ctxs): WsiImage(x[0][i], x[1][i]).show(ctx=ctx)
-#                                                                  
-#
-####TransformBlock###
-#def WsiImageBlock(): return TransformBlock(type_tfms=WsiImage.create, batch_tfms=IntToFloatTensor)
